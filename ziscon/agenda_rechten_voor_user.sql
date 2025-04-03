5d1722d33a3531a38995cc1d186efabf9e97471c
npoppeli <n.poppelier@umcutrecht.nl>
2025-04-03 14:36:44 +0200

    eerste versies voor repository

--- ziscon/agenda_rechten_voor_user.sql
+++ ziscon/agenda_rechten_voor_user.sql
@@ -12,22 +12,17 @@ FROM           [HIX_Acc].[dbo].[ZISCON_GROEPUSR] GU
     inner JOIN [HIX_Acc].[dbo].[ZISCON_GROEPLNK] GL ON ((GU.[GRPUSRCODE] = GL.[LINKCODE]) OR (GU.[USERCODE] = GL.[LINKCODE]))
     inner JOIN [HIX_Acc].[dbo].[ZISCON_GROEPEN]  G  ON GL.[GROEPCODE] = G.[CODE]
 WHERE  GU.[USERCODE] = 'LVOS13'
-
--- USERCODE	GRPUSRCODE	GROEPCODE   OMSCHR
--- LVOS13	ZH001407	ZH1178      Arts in opleiding Interne Geneeskunde (UMC)
--- LVOS13	ZH001407	ZH0003      Aanvullend - Mag inloggen met testgebruikers UMC Utrecht
---
--- Inspectie in HiX: lvos13 is lid van slechts 1 groep, g.code = gl.groepcode = ZH1178. Komt overeen met query-resultaat.
--- Als je van deze groep de agenda-rechten opzoekt, krijg je slechts deze: ZH0021, ZH0022, ZH0023, ZH0025, ZH0026, ZH0027, ZH0028, ZH0086
--- dit is alleen B&O!!!
--- in welke werkcontext-segmenten komt (bijv) ZH0096 (endocrinologie) voor?
--- daarvoor moet je weten wat de parent-groepen, grandparent-groepen, ... van ZH1178 zijn. 
--- dat vind je in ziscon_groeplnk: alle records met linkcode = '@GZH1178' 
-
+ 
 SELECT *
 FROM           [HIX_Acc].[dbo].[ZISCON_GROEPEN] G
     inner JOIN [HIX_Acc].[dbo].[CONFIG_WORKCONTEXT] WC ON G.[CODE] = WC.[OwnerId]
     INNER JOIN [HIX_Acc].[dbo].[CONFIG_WCSEGMENTS] WCS ON WC.[ID] = WCS.[CONFIGUREDWORKCONTEXTID]
+WHERE wc.[SettingId] like 'OND%'
+
+-- settingid                 settingsubid               segmentclassid segmentid
+-- AG*                       ''                         agenda_agenda  agendacode
+-- OND_RECHTEN               functionaliteit in module  nadere aanduiding binnen functionaliteit, bijvoorbeeld vrlcat
+ --
 
 SELECT *
 FROM           [HIX_Acc].[dbo].[ZISCON_GROEPEN] G
