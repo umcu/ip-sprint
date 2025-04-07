845407dec0fd78c6dc091e296981c1bbec71b553
npoppeli <n.poppelier@umcutrecht.nl>
2025-04-07 14:27:25 +0200

    unsaved changes

--- ziscon/agenda_rechten_voor_user.sql
+++ ziscon/agenda_rechten_voor_user.sql
@@ -5,13 +5,13 @@ FROM           [HIX_Acc].[dbo].[ZISCON_GROEPUSR] GU
     inner JOIN [HIX_Acc].[dbo].[CONFIG_WORKCONTEXT] WC ON G.[CODE] = WC.[OwnerId]
     inner JOIN [HIX_Acc].[dbo].[CONFIG_WCSEGMENTS] WCS ON WC.[ID] = WCS.[CONFIGUREDWORKCONTEXTID]
     inner JOIN [HIX_Acc].[dbo].[AGENDA_AGENDA] A ON WCS.[SEGMENTID] = A.[AGENDA]
-WHERE  GU.[USERCODE] = 'LVOS13' and wc.[SettingId] = 'AG_US_BEK'
+WHERE  GU.[USERCODE] = 'TKAPPEN' -- and wc.[SettingId] = 'AG_US_BEK'
 
 SELECT GU.[USERCODE], GU.[GRPUSRCODE], GL.[GROEPCODE], G.[OMSCHR]
 FROM           [HIX_Acc].[dbo].[ZISCON_GROEPUSR] GU
     inner JOIN [HIX_Acc].[dbo].[ZISCON_GROEPLNK] GL ON ((GU.[GRPUSRCODE] = GL.[LINKCODE]) OR (GU.[USERCODE] = GL.[LINKCODE]))
     inner JOIN [HIX_Acc].[dbo].[ZISCON_GROEPEN]  G  ON GL.[GROEPCODE] = G.[CODE]
-WHERE  GU.[USERCODE] = 'LVOS13'
+WHERE  GU.[USERCODE] = 'TKAPPEN'
  
 SELECT *
 FROM           [HIX_Acc].[dbo].[ZISCON_GROEPEN] G
@@ -19,20 +19,9 @@ FROM           [HIX_Acc].[dbo].[ZISCON_GROEPEN] G
     INNER JOIN [HIX_Acc].[dbo].[CONFIG_WCSEGMENTS] WCS ON WC.[ID] = WCS.[CONFIGUREDWORKCONTEXTID]
 WHERE wc.[SettingId] like 'OND%'
 
--- settingid                 settingsubid               segmentclassid segmentid
--- AG*                       ''                         agenda_agenda  agendacode
--- OND_RECHTEN               functionaliteit in module  nadere aanduiding binnen functionaliteit, bijvoorbeeld vrlcat
- --
-
 SELECT *
 FROM           [HIX_Acc].[dbo].[ZISCON_GROEPEN] G
     inner JOIN [HIX_Acc].[dbo].[CONFIG_WORKCONTEXT] WC ON G.[CODE] = WC.[OwnerId]
     INNER JOIN [HIX_Acc].[dbo].[CONFIG_WCSEGMENTS] WCS ON WC.[ID] = WCS.[CONFIGUREDWORKCONTEXTID]
     INNER JOIN [HIX_Acc].[dbo].[AGENDA_AGENDA] A ON WCS.[SEGMENTID] = A.[AGENDA]
     WHERE wc.[SettingId] = 'AG_US_BEK'
-
-
--- exporteren voor data-inspectie met Python
--- SELECT * FROM [HIX_Acc].[dbo].[ZISCON_GROEPEN]
--- SELECT * FROM [HIX_Acc].[dbo].[ZISCON_GROEPUSR]
--- SELECT * FROM [HIX_Acc].[dbo].[ZISCON_GROEPLNK]
